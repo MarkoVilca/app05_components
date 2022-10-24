504203b330552c0efe2fd2a2cddc63f3fb0b06c6
Marco <garishito123@outlook.com>
2022-10-24 12:51:53 -0500

    first commit

---
+++ lib/pages/avatar_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AvatarPage extends StatelessWidget {
+  const AvatarPage({super.key});
+
+  @override
+  Widget build(BuildContext) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Alert Page"),
+      ),
+    );
+  }
+}
